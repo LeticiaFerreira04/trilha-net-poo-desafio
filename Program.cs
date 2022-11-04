350d76c90989755ced281376d9aa67b205b22ebf
Letícia Ferreira <109364561+LeticiaFerreira04@users.noreply.github.com>
2022-11-04 09:55:29 -0300

    Resolução do Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia c = new Nokia("123", "a", "1", 64);
+
+c.InstalarAplicativo("WhatsApp");
+
+Iphone i = new Iphone("246", "b", "3", 128);
+
+i.InstalarAplicativo("Telegram");
